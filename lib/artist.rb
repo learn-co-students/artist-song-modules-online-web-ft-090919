04922bc510598124024134ec68f8c71ed0e9a20e
Randel Jiao <randeljiao@gmail.com>
2019-09-25 22:21:58 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,21 @@
-require 'pry'
-
 class Artist
   attr_accessor :name
   attr_reader :songs
+
   extend Memorable::ClassMethods
+  extend Findable::ClassMethods
   include Memorable::InstanceMethods
-  extend Findable
-  include Paramable
+  include Paramable::InstanceMethods
 
   @@artists = []
 
   def initialize
-    super
+    @@artists << self
     @songs = []
+  end
+
+  def self.find_by_name(name)
+    @@artists.detect{|a| a.name == name}
   end
 
   def self.all
@@ -21,7 +24,7 @@ class Artist
 
   def add_song(song)
     @songs << song
-    song.artist = self
+    song.artist = self unless song.artist
   end
 
   def add_songs(songs)
