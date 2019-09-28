7996d83c804e142d8b6fd37375ad9dabe3fb62b9
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-28 03:55:23 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,17 +3,15 @@ require_relative '../lib/concerns/memorable.rb'
 require_relative '../lib/concerns/findable.rb'
 require_relative '../lib/concerns/paramable.rb'
 class Song
-  extend Memorable
+  extend Memorable::ClassMethods
   extend Findable
+  include Memorable::InstanceMethods
   include Paramable
+  
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
-
-  def initialize
-    @@songs << self
-  end
 
   def self.all
     @@songs
