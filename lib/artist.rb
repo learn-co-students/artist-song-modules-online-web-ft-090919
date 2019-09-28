96de2a579b6fc340f8f3b8666c8ce65d688b45fb
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-28 03:23:29 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,11 @@
 require 'pry'
 require_relative '../lib/concerns/memorable.rb'
+require_relative '../lib/concerns/findable.rb'
+require_relative '../lib/concerns/paramable.rb'
 class Artist
   extend Memorable
+  extend Findable
+  include Paramable
   attr_accessor :name
   attr_reader :songs
 
@@ -12,21 +16,9 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
   def self.all
     @@artists
   end
-
-  #def self.reset_all
-  #  self.all.clear
-  #end
-
-  #def self.count
-  #  self.all.count
-  #end
 
   def add_song(song)
     @songs << song
@@ -37,7 +29,4 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
